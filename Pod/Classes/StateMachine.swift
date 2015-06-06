29657937dbe1442a5b3f67a68a475b153fa46e80
Cesar Tardaguila <cesar.tardaguila@accedo.tv>
2015-06-06 13:54:02 +0200

    Clean header comments

--- Pod/Classes/StateMachine.swift
+++ Pod/Classes/StateMachine.swift
@@ -3,7 +3,6 @@
 //  Turnstile
 //
 //  Created by Cesar Tardaguila on 10/05/15.
-//  Copyright (c) 2015 Accedo Products - Mobile. All rights reserved.
 //
 
 import Foundation
--- Pod/Classes/Transition.swift
+++ Pod/Classes/Transition.swift
@@ -3,7 +3,6 @@
 //  Turnstile
 //
 //  Created by Cesar Tardaguila on 14/05/15.
-//  Copyright (c) 2015 Accedo Products - Mobile. All rights reserved.
 //
 
 public enum Transition {
