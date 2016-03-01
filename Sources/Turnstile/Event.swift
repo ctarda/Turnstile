994d112df7598cceac954d56ca491076ef13ce99
Cesar Tardaguila <ctarda@yyms02.local>
2016-03-01 16:32:16 +0800

    add documentation to Event and State

--- Sources/Turnstile/Event.swift
+++ Sources/Turnstile/Event.swift
@@ -10,11 +10,25 @@ import Foundation
  Events trigger _transitions_ from one _state_ to another. Events link source states with and destination state.
 */
 public class Event <T:Hashable> {
-    
+
+	/**
+		The event name. Read-only
+	*/
     private(set) public final var name: String
+	
+	/**
+		Source States this event can trigger a transtion from. Read-only
+	*/	
     private(set) public final var sourceStates: [State<T>]
+	
+	/**
+		The destination State. Read-only
+	*/	
     private(set) public final var destinationState: State<T>
     
+	/**
+		Creates a new event
+	*/	
     public init(name: String, sourceStates source: [State<T>], destinationState destination: State<T>) {
         self.name = name
         self.sourceStates = source
@@ -23,7 +37,10 @@ public class Event <T:Hashable> {
 }
 
 extension Event: Equatable {}
-
+/**
+	Event comparison. Two events are equal if they have the same name, the same Source States and 
+	the same Destination State
+*/
 public func ==<T>(lhs:Event<T>,rhs:Event<T>) -> Bool {
     return (lhs.name == rhs.name) && (lhs.sourceStates == rhs.sourceStates) && (lhs.destinationState == rhs.destinationState)
 }
--- Sources/Turnstile/State.swift
+++ Sources/Turnstile/State.swift
@@ -10,12 +10,33 @@ import Foundation
  A state is a description of the status of a system at a given moment in time, while it is waiting to execute a _transition_
 */
 public class State <T: Hashable> {
-
+	/**
+		The value encapsulated by the State. Read-only
+	*/
     private(set) final public var value: T
-    
+
+	/**
+		Closure to be called right before the State Machine
+		enters a given State
+	*/
     final public var willEnterState: ((finalState : State<T>) -> Void)?
+	
+	/**
+		Closure to be called right after the State Machine
+		enters a given State
+	*/	
     final public var didEnterState: ((finalState : State<T>) -> Void)?
+	
+	/**
+		Closure to be called right before the State Machine
+		exits a given State
+	*/	
     final public var willExitState: ((initialState : State<T>) -> Void)?
+	
+	/**
+		Closure to be called right after the State Machine
+		exists a given State
+	*/	
     final public var didExitState: ((initialState : State<T>) -> Void)?
     
     public init(value: T) {
@@ -25,6 +46,9 @@ public class State <T: Hashable> {
 
 extension State: Equatable {}
 
+/**
+	Two States are equal if they hold the same value
+*/
 public func ==<T>(lhs:State<T>,rhs:State<T>) -> Bool {
     return (lhs.value == rhs.value)
 }
